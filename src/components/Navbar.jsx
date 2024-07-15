5812b5ec1bd78fa682b5fbc64d60b496434d1e78
Syeong <pyqw_@naver.com>
2024-07-15 17:51:18 +0900

    feat: Add Navbar component
    - Create Navbar.jsx with navigation links
    - Style Navbar with Navbar.css

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import { useNavigate } from 'react-router-dom'
+import '../styles/navbar.css'
+
+function Navbar() {
+    const navigate = useNavigate();
+
+    const moveToHome= () => {
+        navigate('/');
+    }
+
+    const moveToReservation= () => {
+        navigate('/reservation');
+    }
+
+    const moveToTreatment= () => {
+        navigate('/treatment');
+    }
+
+    const moveToLogin = () => {
+        navigate('/login');
+    }
+
+    return (
+        <div className='nav'>
+            <div className="nav-logo">원격진료시스템</div>
+            <ul className="nav-menu">
+                <li className="nav-home" onClick={moveToHome}>홈</li>
+                <li className="nav-reservation" onClick={moveToReservation}>예약</li>
+                <li className="nav-treatment" onClick={moveToTreatment}>진료</li>
+                <li className="nav-login" onClick={moveToLogin}>로그인</li>  
+            </ul>
+        </div>
+    );
+}
+
+export default Navbar
