7c093d61e05617156e566784f6b0ceb7ada49529
Syeong <pyqw_@naver.com>
2024-07-16 12:35:20 +0900

    feat: Create ApptDates components
    - Create ApptDates component to display appointment dates
    - Add functionality to fetch and display appointment dates
    - Add test data in apptDates.json

---
+++ src/components/Appt/ApptDates.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import '../../styles/apptDate.css'
+
+const ApptDates = ({ date }) => {
+    const [appointments, setAppointments] = useState([]);
+
+    useEffect(() => {
+      fetch('/data/apptDates.json')
+        .then(response => response.json())
+        .then(data => setAppointments(data))
+        .catch(error => console.error('Error fetching data:', error));
+    }, []);
+
+    
+      // 선택한 날짜와 일치하는 예약 필터링
+      const filteredAppointments = appointments.filter(
+        (appointment) => appointment.date === date
+      );
+    
+      return (
+        <div className="apptDates">
+          {filteredAppointments.length > 0 ? (
+            <table className="apptTable">
+              <thead>
+                <tr>
+                  <th>교수 성함</th>
+                  <th>날짜</th>
+                  <th>시간</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredAppointments.map((appointment, index) => (
+                  <tr key={index}>
+                    <td>{appointment.professor}</td>
+                    <td>{appointment.date}</td>
+                    <td>{appointment.time}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <p>선택한 날짜에 예약이 없습니다.</p>
+          )}
+        </div>
+      );
+    };
+
+export default ApptDates;
