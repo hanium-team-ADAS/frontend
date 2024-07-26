527dc223d9f734d2eb7c2d88e7a886ae3c083ab8
Syeong <pyqw_@naver.com>
2024-07-26 11:31:51 +0900

    fix: Update available Reservation Dates
    - Changed available reservation dates from (today to 4 weeks from today) to (tomorrow to 4 weeks from tomorrow)

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -2,11 +2,14 @@ export const getMinMaxDates = () => {
   const formatDate = (date) => date.toISOString().split('T')[0];
 
   const today = new Date();
-  const fourWeeksLater = new Date(today);
-  fourWeeksLater.setDate(today.getDate() + 28);
+  const tomorrow = new Date(today);
+  tomorrow.setDate(today.getDate() + 1);
+
+  const fourWeeksLater = new Date(tomorrow);
+  fourWeeksLater.setDate(tomorrow.getDate() + 28);
 
   return { 
-      minDate: formatDate(today),
+      minDate: formatDate(tomorrow),
       maxDate: formatDate(fourWeeksLater) 
   };
 };
