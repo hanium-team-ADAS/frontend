152c0aedd703576bce6bbffa899b5e90623cfab0
Syeong <pyqw_@naver.com>
2024-08-12 15:31:05 +0900

    refactor: Update padding value

--- src/components/PaddingWrapper.jsx
+++ src/components/PaddingWrapper.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 const PaddingWrapper = ({ paddingTop, children }) => {
+  const additionalPadding = 16; // navbars's padding-top = 1rem(=16px)
+
   return (
-    <div style={{ paddingTop: `${paddingTop}px` }}>
+    <div style={{ paddingTop: `${paddingTop + additionalPadding}px` }}>
       {children}
     </div>
   );
